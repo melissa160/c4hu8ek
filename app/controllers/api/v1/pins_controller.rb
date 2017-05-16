798083d89c84f7cb5bbfe8e4911348904fd10796
melissa160 <melissa160@gmail.com>
2017-05-16 14:23:00 -0500

    implementando uso de api con token

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,19 +1,44 @@
 class Api::V1::PinsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :validate_user_and_token
   def index
-    render json: Pin.all.order('created_at DESC')
+    if @valid_keys
+      render json: Pin.all.order('created_at DESC')
+    else
+      head 401
+    end
   end
 
   def create
-    pin = Pin.new(pin_params)
-    if pin.save
-      render json: pin, status: 201
+    if @valid_keys
+      pin = Pin.new(pin_params)
+      if pin.save
+        render json: pin, status: 201
+      else
+        render json: { errors: pin.errors }, status: 422
+      end
     else
-      render json: { errors: pin.errors }, status: 422
+      head 401
     end
   end
 
   private
     def pin_params
-      params.require(:pin).permit(:title, :image_url)
+      params.permit(:title, :image_url).merge(user_id: @user.id)
     end
+
+    def validate_user_and_token
+      #validar si el email existe
+      @valid_keys = false
+      if request.headers["X-User-Email"] && request.headers["X-Api-Token"]
+        @user = User.find_by(email: request.headers["X-User-Email"])  
+        token_is_valid?(@user) if @user
+      end
+      puts @valid_keys
+    end
+
+    def token_is_valid?(user)
+      @valid_keys = true if request.headers["X-Api-Token"] == user.api_token
+    end
+
 end
